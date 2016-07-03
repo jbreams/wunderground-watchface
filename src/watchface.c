ab7c03aaac868671da3bf3d43349baa739af4131
Jonathan Reams <jbreams@gmail.com>
2016-07-03 12:35:34 -0400

    Initial commit

---
+++ src/watchface.c
@@ -0,0 +1,54 @@
+#include <pebble.h>
+
+static Window *s_window;
+static TextLayer *s_time_layer;
+
+static void prv_window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
+
+  s_time_layer = text_layer_create(
+    GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
+  text_layer_set_background_color(s_time_layer, GColorClear);
+  text_layer_set_text_color(s_time_layer, GColorBlack);
+  text_layer_set_text(s_time_layer, "00:00");
+  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+
+  layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
+}
+
+static void tick_handler(struct tm* tick_time, TimeUnits units_changed) {
+  static char time_buffer[128];
+  clock_copy_time_string(time_buffer, (uint8_t)sizeof(time_buffer));
+  text_layer_set_text(s_time_layer, time_buffer);
+}
+
+static void prv_window_unload(Window *window) {
+  text_layer_destroy(s_time_layer);
+}
+
+static void prv_init(void) {
+  s_window = window_create();
+  window_set_window_handlers(s_window, (WindowHandlers) {
+    .load = prv_window_load,
+    .unload = prv_window_unload,
+  });
+
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  const bool animated = true;
+  window_stack_push(s_window, animated);
+}
+
+static void prv_deinit(void) {
+  window_destroy(s_window);
+}
+
+int main(void) {
+  prv_init();
+
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", s_window);
+
+  app_event_loop();
+  prv_deinit();
+}
